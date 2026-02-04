f91327032dcc798f2aca725fea1853fd45ad58bc
sanjanaben <sanjanaben.tandel@simformsolutions.com>
2026-02-04 11:49:09 +0530

    fix: email verification route issue

---
+++ src/components/routes/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Route guard components barrel export
+ */
+export { ProtectedRoute } from "./ProtectedRoute";
+export { PublicRoute } from "./PublicRoute";
+export { UnrestrictedRoute } from "./UnrestrictedRoute";
