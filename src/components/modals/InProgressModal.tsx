8c4612dc5c1d99c45532186f00fd0a2076cdffae
sanjanaben <sanjanaben.tandel@simformsolutions.com>
2026-03-27 17:02:25 +0530

    fix: loader issue

--- src/components/modals/InProgressModal.tsx
+++ src/components/modals/InProgressModal.tsx
@@ -21,35 +21,29 @@ export const InProgressModal = ({
     <Modal isOpen={isOpen} onOpenChange={onClose} size="sm">
       <ModalContent>
         <ModalHeader className="relative flex flex-col items-start gap-4 border-0 pb-0 pt-6 px-6">
-          {/* Featured Icon - Blue Loading Icon */}
-          <div className="flex size-12 items-center justify-center overflow-clip rounded-full shrink-0 bg-blue-300">
-            {/* <div className="size-6 overflow-clip"> */}
-            {/* Flower/asterisk loader - matches the image exactly */}
+          <div className="flex size-12 items-center justify-center rounded-full bg-blue-50 border border-blue-100 shrink-0">
             <svg
               className="size-6 animate-spin"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="none"
             >
-              {/* 8 spokes like a flower/asterisk */}
               {[0, 45, 90, 135, 180, 225, 270, 315].map((angle, i) => (
                 <line
                   key={angle}
                   x1="12"
-                  y1="4"
+                  y1="2"
                   x2="12"
-                  y2="7"
-                  stroke="#3b82f6"
+                  y2="5.5"
+                  stroke="#60a5fa"
                   strokeWidth="2"
                   strokeLinecap="round"
-                  opacity={1 - i * 0.11}
+                  opacity={1 - i * 0.115}
                   transform={`rotate(${angle} 12 12)`}
                 />
               ))}
             </svg>
-            {/* </div> */}
           </div>
-
           {/* <Button
             color="tertiary"
             size="sm"
